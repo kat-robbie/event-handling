100487f6933fba61c18eae445ac56f08adbb1ad6
Kat Robbie <katherine.robbie@gmail.com>
2017-01-19 15:09:11 +1300

    four colourful squares coded

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -7,8 +7,12 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
-    
-  // Your turn! Create a new function called `two`, then call it from here.
+
+  two()// Your turn! Create a new function called `two`, then call it from here.
+
+  three()
+
+  four()
 }
 
 function one () {
@@ -22,11 +26,23 @@ function one () {
   one.addEventListener('mouseleave', makeWhite)
 }
 
-// CREATE FUNCTION two HERE
+function two () {
+  var one = document.getElementById('two')
+  one.addEventListener('mouseenter', makeGreen)
+  one.addEventListener('mouseleave', makeWhite)
+}
 
-// CREATE FUNCTION three HERE
+function three () {
+  var one = document.getElementById('three')
+  one.addEventListener('mouseenter', makePurple)
+  one.addEventListener('mouseleave', makeWhite)
+}
 
-// CREATE FUNCTION four HERE
+function four () {
+  var one = document.getElementById('four')
+  one.addEventListener('click', makeOrange)
+  one.addEventListener('mouseleave', makeWhite)
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
@@ -35,4 +51,16 @@ function makeBlue (evt) {
 
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
+}
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
+}
+
+function makePurple (evt) {
+  evt.target.style.backgroundColor = 'purple'
+}
+
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange'
 }
